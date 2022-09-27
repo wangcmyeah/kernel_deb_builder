85888ae950a87855939f5020d1edfcb39b92a742
wangcmyeah <113437315+wangcmyeah@users.noreply.github.com>
2022-09-27 20:39:21 +0800

    Test_for_6.0-rc7

--- build_action.sh
+++ build_action.sh
@@ -17,7 +17,7 @@ cd "${GITHUB_WORKSPACE}" || exit
 
 # download kernel source
 wget http://www.kernel.org/pub/linux/kernel/v5.x/linux-"$VERSION".tar.xz
-tar -xf linux-"$VERSION".tar.xz
+tar -zxvf linux-"$VERSION".tar.xz
 cd linux-"$VERSION" || exit
 
 # copy config file
